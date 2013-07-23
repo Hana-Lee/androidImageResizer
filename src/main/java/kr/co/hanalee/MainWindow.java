984e302ffdad44185f8236912c19dab99b73eb1b
Hana Lee <voyaging.hana@gmail.com>
2013-07-24 03:23:50 +0900

    Fix 메인 레이아웃 틀 구성

---
+++ src/main/java/kr/co/hanalee/MainWindow.java
@@ -0,0 +1,20 @@
+package kr.co.hanalee;
+
+import javax.swing.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: HanaLee
+ * Date: 13. 7. 24
+ * Time: 오전 2:45
+ * To change this template use File | Settings | File Templates.
+ */
+public class MainWindow {
+    private JPanel mainPanel;
+    private JPanel originPreview;
+    private JPanel resizePreview;
+    private JPanel buttonPanel;
+
+    private void createUIComponents() {
+    }
+}
